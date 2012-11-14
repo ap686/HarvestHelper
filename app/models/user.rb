94f7a232ec2920279d935958ed3253c4492a9db4
Aaron Pagano <Aaron.Pagano@Drexel.edu>
2012-11-13 19:59:37 -0500

    Added user authentication

---
+++ app/models/user.rb
@@ -0,0 +1,21 @@
+class User < ActiveRecord::Base
+  attr_accessible :name, :email, :password, :password_confirmation
+  has_secure_password
+  
+  before_save { |user| user.email = email.downcase }
+  
+  EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  
+  validates :name, 
+              presence: true,
+              length: { maximum: 50 }
+  validates :email, 
+              presence: true,
+              format: { with: EMAIL_REGEX },
+              uniqueness: { case_sensitive: false }
+  validates :password,
+              presence: true,
+              length: { minimum: 5 }
+  validates :password_confirmation,
+              presence: true
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 HarvestHelper::Application.routes.draw do
   root to: 'static_pages#home'
+  
+  resources :users, only: [:show, :new, :create]
+	resources :sessions, only: [:new, :create, :destroy]
+  
   match '/about', to: 'static_pages#about'
+  match '/signup', to: 'users#new'
+	match '/signin', to: 'sessions#new'
+	match '/signout', to: 'sessions#destroy', via: :delete
 end
