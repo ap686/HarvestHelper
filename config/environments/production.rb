78ffaedb4b77ca9f6c699cbc4b97f624addd6450
Aaron Pagano <Aaron.Pagano@Drexel.edu>
2012-11-18 15:45:29 -0500

    Removed ssl for demo

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -65,5 +65,5 @@ HarvestHelper::Application.configure do
   # with SQLite, MySQL, and PostgreSQL)
   # config.active_record.auto_explain_threshold_in_seconds = 0.5
   
-  config.force_ssl = true
+  #config.force_ssl = true
 end
