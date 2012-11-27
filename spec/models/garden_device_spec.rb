a341b4a35f4c6d4ab3b69cf27b975e21dd5d8bf1
stryper <stryper@example.org>
2012-11-26 22:40:44 -0500

    Add more test and change configuration of rspec

--- spec/models/garden_device_spec.rb
+++ spec/models/garden_device_spec.rb
@@ -2,12 +2,16 @@ require "spec_helper"
 
 describe "GardenDevice" do
   describe "create" do
-    let!(:garden_device){FactoryGirl.build(:garden_device)}
+    let!(:garden_device){FactoryGirl.create(:garden_device)}
     
     [:name, :serial_number].each do |attr|
       it "should validate presence of #{attr}" do
         garden_device.should validate_presence_of(attr)
       end
+    end
+    
+    it "should validate uniqueness of serial number" do
+      garden_device.should validate_uniqueness_of(:serial_number)
     end
   end
 end
