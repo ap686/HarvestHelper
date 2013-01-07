edf911118515d6887de966d9ccc4d6e3eca66e68
quocle <stryper@example.org>
2013-01-07 09:59:43 -0500

    Add devise view and fix routing errors

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,23 +1,3 @@
 module SessionsHelper
-  def sign_in(user)
-    session[:user_id] = user.id
-    self.current_user = user
-  end
-  
-  def sign_out
-    self.current_user = nil
-    session[:user_id] = nil
-  end
-  
-  def signed_in?
-    return session[:user_id] != nil
-  end
-  
-  def current_user=(user)
-    @current_user = user
-  end
-  
-  def current_user
-    @current_user ||= User.find(session[:user_id])
-  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ HarvestHelper::Application.routes.draw do
   root to: 'static_pages#home'
   
   #resources :users, only: [:show, :new, :create]
-	resources :sessions, only: [:new, :create, :destroy]
+	#resources :sessions, only: [:new, :create, :destroy]
   resources :garden_devices, only: [:show, :new, :create, :index]
   resources :device_schedules, only: [:create, :destroy]
   resources :message_boards, only: [:index, :show]
@@ -12,9 +12,9 @@ HarvestHelper::Application.routes.draw do
   resources :comments, only: [:new, :create, :destroy]
   
   match '/about', to: 'static_pages#about'
-  match '/signup', to: 'users#new'
-	match '/signin', to: 'sessions#new'
-	match '/signout', to: 'sessions#destroy', via: :delete
+  #match '/signup', to: 'users#new'
+	#match '/signin', to: 'sessions#new'
+	#match '/signout', to: 'sessions#destroy', via: :delete
   match '/add_device', to: 'garden_devices#new'
   
   match '/data_receiver', to: 'garden_device_service#receive_data', via: :post
