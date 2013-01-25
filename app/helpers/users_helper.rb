4d19dd67bed1073ed0dede1aed92f9a8c5381a32
quocle <quocle@some_example.org>
2013-01-25 17:08:00 -0500

    Helper methods

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -2,6 +2,14 @@ module UsersHelper
   def get_full_name
     first = current_user.first_name
     last = current_user.last_name
-    first + " " + last
+    first + " " + last rescue nil
+  end
+  
+  def get_first_name
+    current_user.first_name rescue nil
+  end
+  
+  def get_last_name
+    current_user.last_name rescue nil
   end
 end
