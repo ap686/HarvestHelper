b52d5058c81cecdfc3b638cdd31a206f3f911732
quocle <quocle@example_not_email.org>
2013-01-29 16:45:11 -0500

    validate presence and uniqueness of schedule_type_lookup

--- app/models/schedule_type_lookup.rb
+++ app/models/schedule_type_lookup.rb
@@ -1,5 +1,7 @@
 class ScheduleTypeLookup < ActiveRecord::Base
   attr_accessible :flag, :name
+  validates :name, :presence => true
+  validates :name, :uniqueness => true
   
   def get_name
     self.name ||= "No Name"
