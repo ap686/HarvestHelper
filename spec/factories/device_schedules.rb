8590a2a96af841de5691a01bee446446c6c1dfa6
quocle <quocle@example_not_email.org>
2013-01-29 14:55:39 -0500

    Add sensor_flag to device_schedule factory

--- spec/factories/device_schedules.rb
+++ spec/factories/device_schedules.rb
@@ -1,5 +1,6 @@
 FactoryGirl.define do
   factory :device_schedule do |device_schedule|
     device_schedule.association :garden_device
+    device_schedule.sensor_flag "Device Schedule Flag"
   end
 end
